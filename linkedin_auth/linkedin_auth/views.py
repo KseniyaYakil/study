2af0fe5fa5fb46380ab9348b19725d9624581317
Kseniya Yakil <k.yakil@yandex.ru>
2014-12-07 23:41:05 +0300

    stable version

--- linkedin_auth/linkedin_auth/views.py
+++ linkedin_auth/linkedin_auth/views.py
@@ -5,10 +5,6 @@ import httplib2
 import json
 from urllib import urlencode
 
-#TODO: config .vimrc
-#TODO: correct log
-#TODO: init script
-
 uniq_state = 'ASPKT564VG912S0H5V12M4Z09dwaqsgtfcoi';
 
 def show_user_info(request):
@@ -16,16 +12,26 @@ def show_user_info(request):
 		return HttpResponse("Error: no access_token for this session")
 
 	http_manager = httplib2.Http()
-	#TODO: add several requests to user info
-	req = '{0}{1}?oauth2_access_token={2}'.format(config.Conf.api_addr, config.Conf.api_people, request.session['access_token'])
+	req = '{0}{1}?oauth2_access_token={2}'.format(config.Conf.api_addr,
+												  config.Conf.api_people,
+												  request.session['access_token'])
 	headers, body = http_manager.request(req, 'GET')
 
 	return HttpResponse(body)
 
-
 def get_auth_code(uniq_state):
-	#TODO: rewrite this in readable format
-	return '{0}authorization?response_type=code&client_id={1}&scope={2}&state={3}&redirect_uri={4}'.format(config.Conf.auth_addr, config.AppConf.api_key, config.Conf.scope, uniq_state, config.AppConf.redirect_uri)
+		format_str = '''{0}authorization?
+						response_type=code
+						&client_id={1}
+						&scope={2}
+						&state={3}
+						&redirect_uri={4}'''.format(
+						config.Conf.auth_addr,
+						config.AppConf.api_key,
+						config.Conf.scope,
+						uniq_state,
+						config.AppConf.redirect_uri)
+		return format_str
 
 def get_access_token(request):
 	if 'code' not in request.GET:
@@ -39,16 +45,14 @@ def get_access_token(request):
 		req_params['redirect_uri'] = config.AppConf.redirect_uri
 		req_params['client_secret'] = config.AppConf.api_secret
 
-		access_token_req = '{0}accessToken?'.format(config.Conf.auth_addr)	
-		http_manager = httplib2.Http()
-
+		req_access_token = '{0}accessToken?'.format(config.Conf.auth_addr)
 		req_headers = {'Content-type': 'application/x-www-form-urlencoded'}
-		headers, body = http_manager.request(access_token_req, 'POST', urlencode(req_params), headers=req_headers)
+
+		http_manager = httplib2.Http()
+		headers, body = http_manager.request(req_access_token, 'POST', urlencode(req_params), headers=req_headers)
 
 		recv_answer = json.loads(body)
 		if 'access_token' in recv_answer:
-			#TODO: create access_token as dict!!
-			# + request.session['access_token_exp_at] = time() + recv_answer['expires_in']
 			request.session['access_token'] = recv_answer['access_token']
 			request.session['access_token_exp_in'] = recv_answer['expires_in']
 			return show_user_info(request)
@@ -62,20 +66,18 @@ def get_access_token(request):
 def redirected(request):
 	if 'code' in request.GET and 'state' in request.GET:
 		return get_access_token(request)
-	elif 'error' in request.GET: 
-		return HttpResponse('Auth filed: {0}'.format(request.GET['error_descriprion']))	
-	else: 
+	elif 'error' in request.GET:
+		return HttpResponse('Auth in LinkedIn failed: {0}'.format(request.GET['error_descriprion']))
+	else:
 		raise Http404
 
 def home(request):
 	if 'acces_token' in request.session:
-		resp = 'you have authorised with access token = {0}'.format(request.session['access_token'])
-		return HttpResponse(resp) 
+		return show_user_info(request)
 	else:
-		#TODO: add templates
-		#return render(request, 'linkedin_auth/auth.html', {'auth_addr': get_auth_code(uniq_state)})
 		auth_addr = get_auth_code(uniq_state);
-		response = '<p>Visit LinkedIn auth</p><a href="{0}">LinkedInAuth</a>'.format(auth_addr);
+		response = '''<p>You are not authorized in LinkedIn.</p>
+				      <p>Please, visit
+				      <a href="{0}">LinkedIn authorization page</a></p>'''.format(auth_addr);
 		return HttpResponse(response);
-			
-	
+
